9a45d8d2d66b1f1922ac7145aec17eb68f6e8858
Akrobate <fedorov.artiom@gmail.com>
2025-05-02 23:44:19 +0200

    Added FastLED and buttons definitions

--- code/symfloppy-8266/src/main.cpp
+++ code/symfloppy-8266/src/main.cpp
@@ -5,6 +5,7 @@
 #include <FrequencyGenerator.h>
 #include <secrets.h>
 
+#include <FastLED.h>
 #include <SymfloppyServer.h>
 
 #define SERVER_PORT           80
@@ -14,23 +15,34 @@ const String ssid = "ESP8266_SSID_1";
 
 int channel = DEFAULT_MIDI_CHANNEL;
 
-const byte PIN_BUZZER = 5;
-const byte pinLed = 4;
+const byte PIN_BUZZER = 5;    // GPIO 5   // D1
+const byte PIN_NEOPIXEL_LED = 15;  // GPIO 15  // D8
+const byte PIN_BUTTON_LEFT = 4;    // GPIO 4   // D2
+const byte PIN_BUTTON_RIGHT = 13;   // GPIO 13  // D7
+const byte PIN_BUTTON_MIDDLE = 12;  // GPIO 12  // D6
+
 
 FrequencyGenerator * frequency_generator = new FrequencyGenerator(PIN_BUZZER);
 Player * player = new Player();
 SymfloppyServer * server = new SymfloppyServer(SERVER_PORT);
 
+CRGB leds[3];
+
 
 void setup() {
-
-
-  pinMode(pinLed, OUTPUT);
-
-  digitalWrite(pinLed, LOW);
   
   Serial.begin(115200);
   
+  pinMode(PIN_NEOPIXEL_LED, OUTPUT);
+  digitalWrite(PIN_NEOPIXEL_LED, LOW);
+  for(int i = 0; i < 3; i++) {
+      leds[i] = CRGB::Black;
+  }
+  FastLED.addLeds<NEOPIXEL, PIN_NEOPIXEL_LED>(leds, 3);
+  FastLED.clear();
+  FastLED.delay(1);
+  FastLED.show();
+
   const String mac_address = WiFi.macAddress();
   String mac_serial = WiFi.macAddress();
   String mac_serial_truncated;
@@ -38,15 +50,10 @@ void setup() {
   mac_serial_truncated = mac_serial.substring(0, 4);
 
   delay(1000);
-  // Wifi connection as AP
   WiFi.softAP(ssid);
 
-  // Debug wifi connection as STA
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-  int wifi_max_try = 5;
-
   Serial.print("Connecting");
-  
   int count_try = 0;
   while (WiFi.status() != WL_CONNECTED) {
     delay(200);
@@ -73,9 +80,15 @@ void setup() {
 
   player->onNoteEvent([](Note * note) {
     if (note->isNoteOn()) {
+      leds[0] = CRGB::Green;
+      FastLED.delay(1);
+      FastLED.show();
       frequency_generator->setFrequency(note->getFrequency());
       frequency_generator->start();
     } else {
+      leds[0] = CRGB::Black;
+      FastLED.delay(1);
+      FastLED.show();
       frequency_generator->stop();
     }
   });
